c7a144977d028a8d110408ad22d8958979262635
ChillyVanilly <vasilii.gurev@hs-furtwangen.de>
2020-05-31 13:53:14 +0200

    testing out stuff

--- Aufgabe05/script.js
+++ Aufgabe05/script.js
@@ -54,7 +54,7 @@ let blitz = {
 let eis = {
     name: "LED-Eisschild",
     bild: "Eis.jpg",
-    beschreibung: "Ein großes Eis-LED schild mit verschiendenen Farben zum aufhängen. Batterie und Strombetrieb möglich. Lange LED Lebenszeit von über 50.000 Stunden",
+    beschreibung: "Ein großes Eis-LED schild mit verschiendenen Farben zum aufhängen.\rBatterie und Strombetrieb möglich. Lange LED Lebenszeit von über 50.000 Stunden",
     farbe: "Rosa-Gelb, Rot-Blau, Grün-Gelb, Rosa-Lila, Rot-Lila, Rot-Rosa",
     preis: "35,99€",
     auflager: true,
@@ -71,7 +71,7 @@ let eis = {
 let lippen = {
     name: "LED-Lippen",
     bild: "Lippen.jpg",
-    beschreibung: "LED-Lippenform in verschiedenen Farben zum aufhängen. Batteriebetrieb möglich",
+    beschreibung: "LED-Lippenform in verschiedenen Farben zum aufhängen.\rBatteriebetrieb möglich",
     farbe: "Blau, Rot, Rosa, Lila, Orange",
     preis: "9,99€",
     auflager: true,
@@ -106,7 +106,7 @@ let goodvibes = {
 let hello = {
     name: "LED-Hello Sprechblase Schriftzug",
     bild: "Hello.jpg",
-    beschreibung: "Eine LED Sprechblase mit der Innschrift : Hello. Erhältlich in verschiedenen Farben",
+    beschreibung: "Eine LED Sprechblase mit der Innschrift : Hello.\rErhältlich in verschiedenen Farben",
     farbe: "Rosa-Weiß, Blau-Weiß, Rot-Weiß, Gelb-Weiß",
     preis: "35,99€",
     auflager: true,
@@ -123,7 +123,7 @@ let hello = {
 let bar = {
     name: "Neon-Rosa LED-Bar Schrift",
     bild: "Bar.jpg",
-    beschreibung: "Ein Rosa Neon - Bar Schriftzug. Ideal für zuhause oder für das Gewerbe. Beeindruckt jeden!",
+    beschreibung: "Ein Rosa Neon - Bar Schriftzug.\rIdeal für zuhause oder für das Gewerbe. Beeindruckt jeden!",
     farbe: "Neon-Rosa",
     preis: "39,99€",
     auflager: false,
@@ -140,7 +140,7 @@ let bar = {
 let fuckit = {
     name: "Neon-Rosa LED-FuckIt Schriftzug",
     bild: "Fuckit.jpg",
-    beschreibung: "Eindrucksvoller F*ck it-Schriftzug. Netzteil nicht inbegriffen (benötigt : 5V 6A)",
+    beschreibung: "Eindrucksvoller F*ck it-Schriftzug.\rNetzteil nicht inbegriffen (benötigt : 5V 6A)",
     farbe: "Neon-Rosa",
     preis: "59,90€",
     auflager: true,
@@ -158,7 +158,7 @@ let fuckit = {
 let ladyback = {
     name: "ADV PRO Lady Back Sexy Girls Man Cave Dual Color LED Neonlicht",
     bild: "Ladyback.jpg",
-    beschreibung: "Gesamtgröße: B 300 mm x H 400 mm (W 12 x H 16). Verwenden Sie LED als Lichtquelle, verbrauchen Sie nur 3 Watt, Lebensdauer ist länger als 50.000 Stunden.Stellen Sie eine 1,6 m lange Leitung mit EIN / AUS-Schalter bereit. (EU-Stecker 240 V in die EU) (UK-Stecker 240 V nach UK) (US-Stecker 110 V nach USA und CA) (AU-Stecker 240 V nach AU & NZ)",
+    beschreibung: "Gesamtgröße: B 300 mm x H 400 mm (W 12 x H 16).",
     farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
     preis: "59,99€",
     auflager: true,
@@ -175,7 +175,7 @@ let ladyback = {
 let dragon = {
     name: "ADV PRO Chinese Dragon Room Display Dual Color LED Neonlicht",
     bild: "Dragon.jpg",
-    beschreibung: "Gesamtgröße: B 300 mm x H 400 mm (W 12 x H 16). Verwenden Sie LED als Lichtquelle, verbrauchen Sie nur 3 Watt, Lebensdauer ist länger als 50.000 Stunden.Stellen Sie eine 1,6 m lange Leitung mit EIN / AUS-Schalter bereit. (EU-Stecker 240 V in die EU) (UK-Stecker 240 V nach UK) (US-Stecker 110 V nach USA und CA) (AU-Stecker 240 V nach AU & NZ)",
+    beschreibung: "Gesamtgröße: B 300 mm x H 400 mm (W 12 x H 16).",
     farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
     preis: "59,99€",
     auflager: true,
@@ -192,7 +192,7 @@ let dragon = {
 let dancegirl = {
     name: "ADV PRO Dance Girl Club Bar Pub Dual Color LED Barlicht Neonlicht",
     bild: "Strip.jpg",
-    beschreibung: "Gesamtgröße: B 400 mm x H 600 mm (W 16 x H 24). Verwenden Sie LED als Lichtquelle, verbrauchen Sie nur 3 Watt, Lebensdauer ist länger als 50.000 Stunden.Stellen Sie eine 1,6 m lange Leitung mit EIN / AUS-Schalter bereit. (EU-Stecker 240 V in die EU) (UK-Stecker 240 V nach UK) (US-Stecker 110 V nach USA und CA) (AU-Stecker 240 V nach AU & NZ)",
+    beschreibung: "Gesamtgröße: B 400 mm x H 600 mm (W 16 x H 24).",
     farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
     preis: "119,99€",
     auflager: true,
@@ -209,7 +209,7 @@ let dancegirl = {
 let trust = {
     name: "ADV PRO Trust Me Love Me Fuck Me Décor Man Cave Nightclub Garage Dual Color LED Barlicht Neonlicht",
     bild: "Trust.jpg",
-    beschreibung: "Gesamtgröße: B 600 mm x H 400 mm (W 24 x H 16). Verwenden Sie LED als Lichtquelle, verbrauchen Sie nur 3 Watt, Lebensdauer ist länger als 50.000 Stunden.Stellen Sie eine 1,6 m lange Leitung mit EIN / AUS-Schalter bereit. (EU-Stecker 240 V in die EU) (UK-Stecker 240 V nach UK) (US-Stecker 110 V nach USA und CA) (AU-Stecker 240 V nach AU & NZ)",
+    beschreibung: "Gesamtgröße: B 600 mm x H 400 mm (W 24 x H 16).",
     farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
     preis: "119,99€",
     auflager: true,
@@ -226,7 +226,7 @@ let trust = {
 let fuckoff = {
     name: "ADV PRO Fuck Off Man Cave Garage Dual Color LED Barlicht Neonlicht Lichtwerbung Neon Sign",
     bild: "Fuckoff.jpg",
-    beschreibung: "Gesamtgröße: B 300 mm x H 210 mm (W 12 x H 8,5). Verwenden Sie LED als Lichtquelle, verbrauchen Sie nur 3 Watt, Lebensdauer ist länger als 50.000 Stunden.Stellen Sie eine 1,6 m lange Leitung mit EIN / AUS-Schalter bereit. (EU-Stecker 240 V in die EU) (UK-Stecker 240 V nach UK) (US-Stecker 110 V nach USA und CA) (AU-Stecker 240 V nach AU & NZ)",
+    beschreibung: "Gesamtgröße: B 300 mm x H 210 mm (W 12 x H 8,5).",
     farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
     preis: "89,99€",
     auflager: true,
@@ -243,7 +243,7 @@ let fuckoff = {
 let dowhat = {
     name: "ADV PRO Do What You Love Bedroom Room Home Décor Dual Color LED Barlicht Neonlicht",
     bild: "Dowhat.jpg",
-    beschreibung: "Gesamtgröße: B 400 mm x H 300 mm (W 16 x H 12). Verwenden Sie LED als Lichtquelle, verbrauchen Sie nur 3 Watt, Lebensdauer ist länger als 50.000 Stunden.Stellen Sie eine 1,6 m lange Leitung mit EIN / AUS-Schalter bereit. (EU-Stecker 240 V in die EU) (UK-Stecker 240 V nach UK) (US-Stecker 110 V nach USA und CA) (AU-Stecker 240 V nach AU & NZ)",
+    beschreibung: "Gesamtgröße: B 400 mm x H 300 mm (W 16 x H 12).",
     farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
     preis: "55,99€",
     auflager: true,
@@ -261,7 +261,7 @@ let dowhat = {
 let govee1 = {
     name: "Govee Dreamcolor LED Strip Lichtband, 5M LED Streifen WiFi Drahtlos Handy Streuerbare 5050 LED Band Sync mit Musik, kompatibel mit Alexa, Google Assistant Android iOS (Nicht unterstützt 5G WiFi) [Energieklasse A+]",
     bild: "Govee1.jpg",
-    beschreibung: "[Kompatibel mit ALEXA & Govee APP]: Govee LED Streifen können Ihre Hände befreien, um das Licht zu steuern. Verwenden Sie einfache Gesprächsanfänger, um Ihre Lichter ein- und auszuschalten, Farben einzustellen und die Helligkeit anzupassen. Es ist derzeit kompatibel mit Echo & Google Home.[Drahtlose WiFi Steuerung] Übernehmen Sie die vollständige Kontrolle über Ihre Beleuchtung mit Govee Home APP. Mit der Fernsteuerung können Sie die LED Streifen überall ein- und ausschalten. Die Timer-Funktion des WiFi-Kontrollers lässt Sie in Ihrer Lieblingsfarbe aufwachen, damit Sie sich auf dem Zeitplan halten können.[IC Control Lampenperlen] Einzigartiges Design durch Hinzufügen von IC in Lichtstreifen, eine Vielzahl von Farben und Helligkeit kann gleichzeitig mit Frequenzänderungen geändert werden. Erweitern Sie die Schönheit und Eleganz von Farbenpracht in Ihrem ganzen Haus.[Multi Verwendungen] Govee RGB & Dreamcolor LED Lichtleiste kann zur Dekoration von Esszimmer, Schlafzimmer, Obergeschoss, Küche, Veranda, Schreibtisch und Wohnräumen verwendet werden, besonders gut für Feiertage und Veranstaltungen wie Weihnachten, Halloween, Partys und mehr. Es ist ein großartiges Geschenk für Ihre Familie und Freunde.[Einfache Installation] Alles in einem Kit, einem WiFi-Kontroller und einem 5M LED Streifen, Original 3M-Kleber und 5 Befestigungsklemme. Govee bietet eine einfache Möglichkeit, um Ihr vernetztes Haus zu entwerfen. Unsere App wird ständig aktualisiert, um unser Produkt mit intelligenteren Geräten kompatibel zu machen.",
+    beschreibung: "[Kompatibel mit ALEXA & Govee APP]: Govee LED Streifen",
     farbe: "Dreamcolor",
     länge: "5M",
     wasserdicht: "Wasserdicht",
@@ -278,7 +278,7 @@ let govee1 = {
 let govee2 = {
     name: "Govee Alexa LED Strip Lichtband, 5M RGB Smart WiFi LED Streifen,APP Steuerbar Musik LED Band Lichterkette für Haus, Küche, TV, Party,kompatibel mit Alexa, Google Assistant (Nicht unterstützt 5G WiFi) [Energieklasse A+]",
     bild: "Govee2.jpg",
-    beschreibung: "[Sprachsteuerung über Alexa & Google Assistant] Govee LED streifen strip können Ihre Hände befreien, um das Licht zu steuern. Verwenden Sie einfache Gesprächsanfänger, um Ihre Lichter ein- und auszuschalten, Farben einzustellen und die Helligkeit anzupassen. Es ist derzeit kompatibel mit Echo & Google Home.[Drahtlose WiFi Steuerung] Übernehmen Sie die vollständige Kontrolle über Ihre Beleuchtung mit „Govee APP“. Mit der Fernsteuerung können Sie die LED Streifen überall ein- und ausschalten. Die Timer-Funktion des WiFi-Kontrollers lässt Sie in Ihrer Lieblingsfarbe aufwachen, damit Sie sich auf dem Zeitplan halten können.[Musik LED Strip Lichtband] Es gibt ein eingebautes Mikrofon, das sich mit jeder Musik synchronisiert, die aus dem Telefonlautsprecher oder einem anderen Lautsprecher im Raum kommt, und die Lichter zusammen mit Musikschlägen(oder Hände klatschen) tanzt. Diese Funktion ist in der App und im Controller verfügbar. Nutzen sie es, um ihre umgebung lebendig zu machen![Perfektes Ambiente Licht] Unsere Lichtleisten bestehen aus hochwertigen, hellen 5050 LEDs. Es ist NICHT wasserdicht und wurde für den Innenbereich entwickelt. Es kann Ihr Schlafzimmer, Ihre Decke, Ihre Treppe, Ihren Küchenschrank, Ihre Veranda, Ihren Schreibtisch und Ihr Wohnzimmer dekorieren, besonders gut für Feiertage wie Weihnachten, Halloween, Partys etc.[Einfache Installation] Alles in einem Kit, mit einem von CE zugelassenen Adapter, einem WiFi-Kontroller und einem 5M LED Streifen, Original 3M-Kleber und 5 Befestigung Klemme. Govee bietet eine einfache Möglichkeit, um Ihr vernetztes Haus zu entwerfen. Unsere App wird ständig aktualisiert, um unser Produkt mit intelligenteren Geräten kompatibel zu machen.",
+    beschreibung: "[Sprachsteuerung über Alexa & Google Assistant] Govee LED streifen strip",
     farbe: "RGB",
     länge: "5M",
     wasserdicht: "Nicht Wasserdicht",
@@ -295,7 +295,7 @@ let govee2 = {
 let govee3 = {
     name: "Govee 2 x 5m RGB LED Streifen Lichtband mit 3 in 1 App, Fernbedienung und Steuerbox, Selbstklebend LED Strip Beleuchtung Full Kit mit DIY-Timer-Einstellung für Zuhause, Schlafzimmer, TV, Schrankdeko [Energieklasse A+]",
     bild: "Govee3.jpg",
-    beschreibung: "Holen Sie sich das ganze Haus Vibrant: Govee 2 x 5m LED streifen sind mit mehr als 16 Millionen Farben erhältlich und bieten lebendige Beleuchtung für die Inneneinrichtung. Mehrere Szenen, die optional in der App verfügbar sind, machen Ihren besonderen Moment unvergesslicher. Im DIY-Modus macht das Spielen mit hellen Farben und Effekten Spaß.Intelligente Dreifachsteuerung: Govee RGB band Sie können die Beleuchtung auf drei Arten vollständig steuern: über die Steuerbox, die Fernbedienung und die Govee Home App. Schalten Sie das Licht ein / aus, dimmen Sie es auf die gewünschte Stufe oder ändern Sie die Farben nach Ihren Wünschen. Bis zu 10 m App-Steuerung über Bluetooth ermöglichen die Steuerung an Ihren Fingerspitzen.Einrichtung in Sekunden: Kleben Sie das Lichtband einfach auf eine saubere, trockene Oberfläche und nehmen Sie es in Betrieb. Schneidendes Design ermöglicht es Ihnen, es über Schneidemarkierung zu trimmen und zu dekorieren, wo immer Sie wollen. Leistungsstarke 3M selbstklebend und 10 Clips halten die Lampen sicher in Position.Überragende Qualität: Zuverlässige Farbwechsel LED Strip bieten eine gute Leistung bei Helligkeit und Farbwiedergabe. Mit einer Lebensdauer von bis zu 50.000 Jahren eignet es sich ideal für Wohnzimmer, Schlafzimmer, Küche, Festivals, Partys usw. Malen Sie Ihr Leben mit lebendigen, abwechslungsreichen Farben.Sicher in der Anwendung: 12 V Spannung betrieben und gibt extrem wenig Wärme ab. Berührbar auch für kleine Kinder. Die integrierte Kurzschlussverhütung bietet doppelten Schutz. Ihre Sicherheit steht an erster Stelle.",
+    beschreibung: "Holen Sie sich das ganze Haus Vibrant: Govee 2 x 5m LED streifen sind mit mehr als 16 Millionen Farben erhältlich",
     farbe: "RGB",
     länge: "2x 5M",
     wasserdicht: "Wasserdicht",
@@ -312,7 +312,7 @@ let govee3 = {
 let ws2812b1 = {
     name: "Longruner WS2812B LED Strip Panel Kit Matrix 8x32 256 Pixel Digitales Flexibles Integriertes WS2812B IC LED Licht mit Voller Traumfarbenbeleuchtung DC5V LWS03 (WS03)",
     bild: "ws2812b.jpg",
-    beschreibung: "Sonderfunktion: Jede LED kann individuell adressiert werden. Sie können jede LED so einstellen, dass Nachrichten gescrollt oder kleine Bilder gezeichnet werden. Bitte verwenden Sie unseren Codetest, bevor Sie Folgendes verwenden: https://mega.nz/#F!uVM3EJZb!JJ7D-TQbGAJDU7bL-RWVxwAnwendung: Breite Kompatibilität. Es funktioniert hervorragend mit Arduino, Raspberry Pi, FastLED-Bibliothek, Adafruit NeoPixel-Bibliothek, Rainbowduino, SP103E, SP105E, K1000C, T1000S usw.Design: Sie können das Panel erweitern, indem Sie es einzeln mit den 3-Pin-JST-Anschlüssen verbinden. Flexibles FPCB kann leicht um Flächen gebogen und gebogen werden.Breite anwendung: es kann verwendet werden, um led-bildschirm, led wand, werbung board und weit verbreitet auf hotel, ktv, bars, außenwerbung zeichen, weihnachten oder hochzeit party dekoration, etc.",
+    beschreibung: "Sonderfunktion: Jede LED kann individuell adressiert werden. Sie können jede LED so einstellen, dass Nachrichten gescrollt oder kleine Bilder gezeichnet werden.",
     farbe: "Dreamcolor",
     länge: "8x32 256 Pixel",
     wasserdicht: "Nicht Wasserdicht",
@@ -330,41 +330,41 @@ let array = [govee1, govee2, govee3, ws2812b1, flamingo, kaktus, blitz, eis, lip
 for (let i = 0; i < array.length; i++) {
     if (array[i].stripe) {
         let div = document.createElement("div");
-        div.id = "produktStripe" + i;
-        document.getElementById("Stripe")?.appendChild(div);
+        div.id = "produktstripe" + i;
+        document.getElementById("stripe")?.appendChild(div);
         let bildStreifen = document.createElement("img");
         bildStreifen.src = array[i].bild;
-        document.getElementById("produktStripe" + i)?.appendChild(bildStreifen);
+        document.getElementById("produktstripe" + i)?.appendChild(bildStreifen);
         let nameStreifen = document.createElement("h3");
         nameStreifen.innerHTML = array[i].name;
-        document.getElementById("produkStripe" + i)?.appendChild(nameStreifen);
+        document.getElementById("produkstripe" + i)?.appendChild(nameStreifen);
         let beschreibungStreifen = document.createElement("p");
         beschreibungStreifen.innerHTML = array[i].beschreibung;
-        document.getElementById("produktStripe" + i)?.appendChild(beschreibungStreifen);
+        document.getElementById("produktstripe" + i)?.appendChild(beschreibungStreifen);
         let farbeStreifen = document.createElement("p");
         farbeStreifen.innerHTML = array[i].farbe;
-        document.getElementById("produktStripe" + i)?.appendChild(farbeStreifen);
+        document.getElementById("produktstripe" + i)?.appendChild(farbeStreifen);
         let längeStreifen = document.createElement("p");
         längeStreifen.innerHTML = array[i].länge;
         document.getElementById("produktStripe" + i)?.appendChild(längeStreifen);
         let wasserdichtStreifen = document.createElement("p");
         wasserdichtStreifen.innerHTML = array[i].wasserdicht;
-        document.getElementById("produktStripe" + i)?.appendChild(wasserdichtStreifen);
+        document.getElementById("produktstripe" + i)?.appendChild(wasserdichtStreifen);
         let wifiStreifen = document.createElement("p");
         wifiStreifen.innerHTML = array[i].wifi;
-        document.getElementById("produktStripe" + i)?.appendChild(wifiStreifen);
+        document.getElementById("produktstripe" + i)?.appendChild(wifiStreifen);
         let smarthomeStreifen = document.createElement("p");
         smarthomeStreifen.innerHTML = array[i].smarthome;
-        document.getElementById("produktStripe" + i)?.appendChild(smarthomeStreifen);
+        document.getElementById("produktstripe" + i)?.appendChild(smarthomeStreifen);
         let programmierStreifen = document.createElement("p");
         programmierStreifen.innerHTML = array[i].programmierbar;
-        document.getElementById("produktStripe" + i)?.appendChild(programmierStreifen);
+        document.getElementById("produktstripe" + i)?.appendChild(programmierStreifen);
         let preisStreifen = document.createElement("h4");
         preisStreifen.innerHTML = array[i].preis;
-        document.getElementById("produktStripe" + i)?.appendChild(preisStreifen);
+        document.getElementById("produktstripe" + i)?.appendChild(preisStreifen);
         let button = document.createElement("button");
         document.getElementById(array[i].name)?.appendChild(button);
-        document.getElementById("produktStripe" + i)?.appendChild(button);
+        document.getElementById("produktstripe" + i)?.appendChild(button);
         button.innerHTML = "In den Warenkorb";
     }
     /*else if (array[i].figur) {
@@ -437,7 +437,7 @@ for (let i = 0; i < array.length; i++) {
         button.innerHTML = "In den Warenkorb";
 
     }
-   /* else if (array[i].text) {
+    else if (array[i].text) {
         let  div: HTMLDivElement = document.createElement("div");
         div.id = "produktText" + i;
         document.getElementById("Text")?.appendChild(div);
