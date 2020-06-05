c6a51dd2986af14e78724dc90402177dd481fbbc
ChillyVanilly <vasilii.gurev@hs-furtwangen.de>
2020-06-05 19:35:13 +0200

    added correction of shop aufgabe 05

---
+++ Shop/script.ts
@@ -0,0 +1,526 @@
+interface Produkte {
+    name: string;
+    bild: string;
+    beschreibung: string;
+    farbe: string;
+    preis: string;
+    auflager: boolean;
+    programmierbar: string;
+    figur: boolean;
+    text: boolean;
+    schild: boolean;
+    stripe: boolean;
+    länge: string;
+    wifi: string;
+    wasserdicht: string;
+    smarthome: string;
+
+}
+
+
+//Figuren
+
+let kaktus: Produkte = {
+    name: "LED-Kaktus",
+    bild: "Items/kaktus.jpg",
+    beschreibung: "Ein grüner hell leuchtender LED-Kaktus! Eine Dekorative Bereicherung für alle Inneneinrichtungen.",
+    farbe: "Grün",
+    preis: "12,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: true,
+    text: false,
+    schild: false,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+};
+
+let flamingo: Produkte = {
+    name: "LED-Flamingo",
+    bild: "Items/Flamingo.jpg",
+    beschreibung: "Eine rosa leuchtende prächtige LED-Flamingofigur",
+    farbe: "Rosa",
+    preis: "10,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: true,
+    text: false,
+    schild: false,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+
+
+};
+
+let blitz: Produkte = {
+    name: "LED-Blitz",
+    bild: "Items/Blitz.jpg",
+    beschreibung: "Eine LED-Blitz-Form zum aufhängen oder hinstellen. Eignet sich auch draußen",
+    farbe: "Blau, Rot, Gelb, Rosa, Lila",
+    preis: "15,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: true,
+    text: false,
+    schild: false,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+
+    
+};
+
+let eis: Produkte = {
+    name: "LED-Eisschild",
+    bild: "Items/Eis.jpg",
+    beschreibung: "Ein großes Eis-LED schild mit verschiendenen Farben zum aufhängen.\rBatterie und Strombetrieb möglich. Lange LED Lebenszeit von über 50.000 Stunden",
+    farbe: "Rosa-Gelb, Rot-Blau, Grün-Gelb, Rosa-Lila, Rot-Lila, Rot-Rosa",
+    preis: "35,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: true,
+    text: false,
+    schild: false,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+
+};
+
+let lippen: Produkte = {
+    name: "LED-Lippen",
+    bild: "Items/Lippen.jpg",
+    beschreibung: "LED-Lippenform in verschiedenen Farben zum aufhängen.\rBatteriebetrieb möglich",
+    farbe: "Blau, Rot, Rosa, Lila, Orange",
+    preis: "9,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: true,
+    text: false,
+    schild: false,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",    
+    smarthome: ""
+
+
+};
+
+//Texte
+
+let goodvibes: Produkte = {
+    name: "LED-GoodVibes Schriftzug",
+    bild: "Items/GoodVibes.jpg",
+    beschreibung: "Ein Blauer-LED Good Vibes Schriftzug zum hinstellen oder aufhängen",
+    farbe: "Blau, Rot, Rosa",
+    preis: "36,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: false,
+    text: true,
+    schild: false,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+
+
+};
+
+let hello: Produkte = {
+    name: "LED-Hello Sprechblase Schriftzug",
+    bild: "Items/Hello.jpg",
+    beschreibung: "Eine LED Sprechblase mit der Innschrift : Hello.\rErhältlich in verschiedenen Farben",
+    farbe: "Rosa-Weiß, Blau-Weiß, Rot-Weiß, Gelb-Weiß",
+    preis: "35,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: false,
+    text: true,
+    schild: false,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+};
+
+let bar: Produkte = {
+    name: "Neon-Rosa LED-Bar Schrift",
+    bild: "Items/Bar.jpg",
+    beschreibung: "Ein Rosa Neon - Bar Schriftzug.\rIdeal für zuhause oder für das Gewerbe. Beeindruckt jeden!",
+    farbe: "Neon-Rosa",
+    preis: "39,99€",
+    auflager: false,
+    programmierbar: "",
+    figur: false,
+    text: true,
+    schild: false,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+};
+
+let fuckit: Produkte = {
+    name: "Neon-Rosa LED-FuckIt Schriftzug",
+    bild: "Items/Fuckit.jpg",
+    beschreibung: "Eindrucksvoller F*ck it-Schriftzug.\rNetzteil nicht inbegriffen (benötigt : 5V 6A)",
+    farbe: "Neon-Rosa",
+    preis: "59,90€",
+    auflager: true,
+    programmierbar: "",
+    figur: false,
+    text: true,
+    schild: false,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+};
+
+//LED-Schilder
+
+let ladyback: Produkte = {
+    name: "ADV PRO Lady Back Sexy Girls Man Cave Dual Color LED Neonlicht",
+    bild: "Items/Ladyback.jpg",
+    beschreibung: "Gesamtgröße: B 300 mm x H 400 mm (W 12 x H 16).",
+    farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
+    preis: "59,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: false,
+    text: false,
+    schild: true,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+};
+
+let dragon: Produkte = {
+    name: "ADV PRO Chinese Dragon Room Display Dual Color LED Neonlicht",
+    bild: "Items/Dragon.jpg",
+    beschreibung: "Gesamtgröße: B 300 mm x H 400 mm (W 12 x H 16).",
+    farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
+    preis: "59,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: false,
+    text: false,
+    schild: true,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+};
+
+let dancegirl: Produkte = {
+    name: "ADV PRO Dance Girl Club Bar Pub Dual Color LED Barlicht Neonlicht",
+    bild: "Items/Strip.jpg",
+    beschreibung: "Gesamtgröße: B 400 mm x H 600 mm (W 16 x H 24).",
+    farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
+    preis: "119,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: false,
+    text: false,
+    schild: true,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+
+};
+
+let trust: Produkte = {
+    name: "ADV PRO Trust Me Love Me Fuck Me Décor Man Cave Nightclub Garage Dual Color LED Barlicht Neonlicht",
+    bild: "Items/Trust.jpg",
+    beschreibung: "Gesamtgröße: B 600 mm x H 400 mm (W 24 x H 16).",
+    farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
+    preis: "119,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: false,
+    text: false,
+    schild: true,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+};
+
+let fuckoff: Produkte = {
+    name: "ADV PRO Fuck Off Man Cave Garage Dual Color LED Barlicht Neonlicht Lichtwerbung Neon Sign",
+    bild: "Items/Fuckoff.jpg",
+    beschreibung: "Gesamtgröße: B 300 mm x H 210 mm (W 12 x H 8,5).",
+    farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
+    preis: "89,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: false,
+    text: false,
+    schild: true,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+    
+};
+
+let dowhat: Produkte = {
+    name: "ADV PRO Do What You Love Bedroom Room Home Décor Dual Color LED Barlicht Neonlicht",
+    bild: "Items/Dowhat.jpg",
+    beschreibung: "Gesamtgröße: B 400 mm x H 300 mm (W 16 x H 12).",
+    farbe: "Blau-Rot, Blau-Weiß, Rot-Weiß, Rot-Rosa, Gelb-Weiß, Gelb-Orange, Blau-Grün, Grün-Weiß, Rosa-Weiß, Rosa-Lila, Blau-Lila, Lila-Weiß",
+    preis: "55,99€",
+    auflager: true,
+    programmierbar: "",
+    figur: false,
+    text: false,
+    schild: true,
+    stripe: false,
+    wasserdicht: "",
+    wifi: "",
+    länge: "",
+    smarthome: ""
+
+};
+
+//LED-Stripes
+
+let govee1: Produkte = {
+
+    name: "Govee Dreamcolor LED Strip Lichtband, 5M LED Streifen WiFi",
+    bild: "Items/Govee1.jpg",
+    beschreibung: "[Kompatibel mit ALEXA & Govee APP]",
+    farbe: "Dreamcolor",
+    länge: "5M",
+    wasserdicht: "Wasserdicht",
+    wifi: "WIFI-Compatibel",
+    smarthome: "Smarthome System Compatibel",
+    preis: "30,99€",
+    auflager: true,
+    programmierbar: "Nicht Programmierbar",
+    figur: false,
+    text: false,
+    schild: false,
+    stripe: true
+
+};
+
+let govee2: Produkte = {
+    name: "Govee Alexa LED Strip Lichtband, 5M RGB Smart WiFi LED Streifen",
+    bild: "Items/Govee2.jpg",
+    beschreibung: "[Sprachsteuerung über Alexa & Google Assistant]",
+    farbe: "RGB",
+    länge: "5M",
+    wasserdicht: "Nicht Wasserdicht",
+    wifi: "WIFI-Compatibel",
+    smarthome: "Smarthome System Compatibel",
+    preis: "28,99€",
+    auflager: true,
+    programmierbar: "Nicht Programmierbar",
+    figur: false,
+    text: false,
+    schild: false,
+    stripe: true
+};
+
+let govee3: Produkte = {
+    name: "Govee 2 x 5m RGB LED Streifen Lichtband mit 3 in 1 App",
+    bild: "Items/Govee3.jpg",
+    beschreibung: "Govee 2 x 5m LED streifen",
+    farbe: "RGB",
+    länge: "2x 5M",
+    wasserdicht: "Wasserdicht",
+    wifi: "WIFI-Compatibel",
+    smarthome: "Smarthome - Nicht Compatibel",
+    preis: "39,99€",
+    auflager: true,
+    programmierbar: "Nicht Programmierbar",
+    figur: false,
+    text: false,
+    schild: false,
+    stripe: true
+};
+
+
+let array: Array<Produkte> = [govee1, govee2, govee3, flamingo, kaktus, blitz, eis, lippen, bar, fuckit, hello, goodvibes, ladyback, fuckoff, dragon, dowhat, dancegirl, trust];
+for (let i: number = 0; i < array.length; i++) {
+    if (array[i].stripe) {
+        let  div: HTMLDivElement = document.createElement("div");
+        div.id = "produktstripe" + i;
+        document.getElementById("stripe")?.appendChild(div);
+       
+        let bildstripe: HTMLImageElement = document.createElement("img");
+        bildstripe.src = array[i].bild;
+        document.getElementById("produktstripe" + i)?.appendChild(bildstripe);
+       
+        let namestripe: HTMLElement = document.createElement("h1");
+        namestripe.innerHTML = array[i].name;
+        document.getElementById("produkstripe" + i)?.appendChild(namestripe);
+ 
+        let beschreibungstripe: HTMLElement = document.createElement("p");
+        beschreibungstripe.innerHTML = array[i].beschreibung;
+        document.getElementById("produktstripe" + i)?.appendChild(beschreibungstripe);
+
+        let farbestripe: HTMLElement = document.createElement("p");
+        farbestripe.innerHTML = array[i].farbe;
+        document.getElementById("produktstripe" + i)?.appendChild(farbestripe);
+
+        let längestripe: HTMLElement = document.createElement("p");
+        längestripe.innerHTML = array[i].länge;
+        document.getElementById("produktstripe" + i)?.appendChild(längestripe);
+
+        let wasserdichtstripe: HTMLElement = document.createElement("p");
+        wasserdichtstripe.innerHTML = array[i].wasserdicht;
+        document.getElementById("produktstripe" + i)?.appendChild(wasserdichtstripe);
+
+        let wifistripe: HTMLElement = document.createElement("p");
+        wifistripe.innerHTML = array[i].wifi;
+        document.getElementById("produktstripe" + i)?.appendChild(wifistripe);
+
+        let smarthomestripe: HTMLElement = document.createElement("p");
+        smarthomestripe.innerHTML = array[i].smarthome;
+        document.getElementById("produktstripe" + i)?.appendChild(smarthomestripe);
+
+        let programmierstripe: HTMLElement = document.createElement("p");
+        programmierstripe.innerHTML = array[i].programmierbar;
+        document.getElementById("produktstripe" + i)?.appendChild(programmierstripe);
+   
+        let preisstripe: HTMLElement = document.createElement("h2");
+        preisstripe.innerHTML = array[i].preis;
+        document.getElementById("produktstripe" + i)?.appendChild(preisstripe);
+           
+        let button: HTMLElement = document.createElement("button");
+        document.getElementById(array[i].name)?.appendChild(button);
+        document.getElementById("produktstripe" + i)?.appendChild(button);
+        button.innerHTML = "In den Warenkorb";
+    }
+
+    else if (array[i].figur) {
+        let  div: HTMLDivElement = document.createElement("div");
+        div.id = "produktfigur" + i;
+        document.getElementById("figur")?.appendChild(div);
+       
+        let bildfigur: HTMLImageElement = document.createElement("img");
+        bildfigur.src = array[i].bild;
+        document.getElementById("produktfigur" + i)?.appendChild(bildfigur);
+       
+        let namefigur: HTMLElement = document.createElement("h1");
+        namefigur.innerHTML = array[i].name;
+        document.getElementById("produkfigur" + i)?.appendChild(namefigur);
+ 
+        let beschreibungfigur: HTMLElement = document.createElement("p");
+        beschreibungfigur.innerHTML = array[i].beschreibung;
+        document.getElementById("produktfigur" + i)?.appendChild(beschreibungfigur);
+
+        let farbefigur: HTMLElement = document.createElement("p");
+        farbefigur.innerHTML = array[i].farbe;
+        document.getElementById("produktfigur" + i)?.appendChild(farbefigur);
+
+        let wasserdichtfigur: HTMLElement = document.createElement("p");
+        wasserdichtfigur.innerHTML = array[i].wasserdicht;
+        document.getElementById("produktfigur" + i)?.appendChild(wasserdichtfigur);
+   
+        let preisfigur: HTMLElement = document.createElement("h2");
+        preisfigur.innerHTML = array[i].preis;
+        document.getElementById("produktfigur" + i)?.appendChild(preisfigur);
+           
+        let button: HTMLElement = document.createElement("button");
+        document.getElementById(array[i].name)?.appendChild(button);
+        document.getElementById("produktfigur" + i)?.appendChild(button);
+        button.innerHTML = "In den Warenkorb";
+    }
+    else if (array[i].schild) {
+
+        let  div: HTMLDivElement = document.createElement("div");
+        div.id = "produktschild" + i;
+        document.getElementById("schild")?.appendChild(div);
+       
+        let bildschild: HTMLImageElement = document.createElement("img");
+        bildschild.src = array[i].bild;
+        document.getElementById("produktschild" + i)?.appendChild(bildschild);
+       
+        let nameschild: HTMLElement = document.createElement("h1");
+        nameschild.innerHTML = array[i].name;
+        document.getElementById("produkschild" + i)?.appendChild(nameschild);
+ 
+        let beschreibungschild: HTMLElement = document.createElement("p");
+        beschreibungschild.innerHTML = array[i].beschreibung;
+        document.getElementById("produktschild" + i)?.appendChild(beschreibungschild);
+
+        let farbeschild: HTMLElement = document.createElement("p");
+        farbeschild.innerHTML = array[i].farbe;
+        document.getElementById("produktschild" + i)?.appendChild(farbeschild);
+
+        let wasserdichtschild: HTMLElement = document.createElement("p");
+        wasserdichtschild.innerHTML = array[i].wasserdicht;
+        document.getElementById("produktschild" + i)?.appendChild(wasserdichtschild);
+   
+        let preisschild: HTMLElement = document.createElement("h2");
+        preisschild.innerHTML = array[i].preis;
+        document.getElementById("produktschild" + i)?.appendChild(preisschild);
+           
+        let button: HTMLElement = document.createElement("button");
+        document.getElementById(array[i].name)?.appendChild(button);
+        document.getElementById("produktschild" + i)?.appendChild(button);
+        button.innerHTML = "In den Warenkorb";
+
+    }
+    else if (array[i].text) {
+        let  div: HTMLDivElement = document.createElement("div");
+        div.id = "produkttext" + i;
+        document.getElementById("text")?.appendChild(div);
+       
+        let bildtext: HTMLImageElement = document.createElement("img");
+        bildtext.src = array[i].bild;
+        document.getElementById("produkttext" + i)?.appendChild(bildtext);
+       
+        let nametext: HTMLElement = document.createElement("h1");
+        nametext.innerHTML = array[i].name;
+        document.getElementById("produktext" + i)?.appendChild(nametext);
+ 
+        let beschreibungtext: HTMLElement = document.createElement("p");
+        beschreibungtext.innerHTML = array[i].beschreibung;
+        document.getElementById("produkttext" + i)?.appendChild(beschreibungtext);
+
+        let farbetext: HTMLElement = document.createElement("p");
+        farbetext.innerHTML = array[i].farbe;
+        document.getElementById("produkttext" + i)?.appendChild(farbetext);
+
+        let wasserdichttext: HTMLElement = document.createElement("p");
+        wasserdichttext.innerHTML = array[i].wasserdicht;
+        document.getElementById("produkttext" + i)?.appendChild(wasserdichttext);
+   
+        let preistext: HTMLElement = document.createElement("h2");
+        preistext.innerHTML = array[i].preis;
+        document.getElementById("produkttext" + i)?.appendChild(preistext);
+           
+        let button: HTMLElement = document.createElement("button");
+        document.getElementById(array[i].name)?.appendChild(button);
+        document.getElementById("produkttext" + i)?.appendChild(button);
+        button.innerHTML = "In den Warenkorb";
+
+    }
+}
