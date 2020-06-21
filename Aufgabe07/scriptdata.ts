d015bb8ee5d955babdd757b3bbb7129d5c892027
ChillyVanilly <vasilii.gurev@hs-furtwangen.de>
2020-06-21 12:44:41 +0200

    Added Data.json

---
+++ Aufgabe07/scriptdata.ts
@@ -0,0 +1,25 @@
+namespace Aufgabe07 {
+    export interface Artikel {
+        name: string;
+        bild: string;
+        beschreibung: string;
+        farbe: string;
+        preis: number;
+        auflager: boolean;
+        programmierbar: string;
+        category: number;
+        länge: string;
+        wifi: string;
+        wasserdicht: string;
+        smarthome: string;
+    }
+    export let artikel: Artikel[];
+    loadArtikel("data.json");
+
+    async function loadArtikel(_url: RequestInfo): Promise<void> {
+        let response: Response = await fetch(_url);
+        let jsonArray: JSON = await response.json();
+        artikel = await JSON.parse(JSON.stringify(jsonArray));
+        createArtikel();
+    }
+}
