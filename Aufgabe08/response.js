1344a7b6cfd070320d990d65040065cf9639e4b3
ChillyVanilly <vasilii.gurev@hs-furtwangen.de>
2020-06-23 23:12:51 +0200

    finished

--- Aufgabe08/response.js
+++ Aufgabe08/response.js
@@ -1,13 +1,19 @@
 "use strict";
 var Aufgabe08;
 (function (Aufgabe08) {
+    //Gibt Button einen Eventlistener bei Click
     document.getElementById("buttonid")?.addEventListener("click", ausgeben);
     async function ausgeben() {
+        //greift auf die Daten des forms im login.html zurück und packt sie in formDatra
         let formData = new FormData(document.forms[0]);
+        //Server url als String
         let url = "https://vasilii-server.herokuapp.com/";
+        //Quer : Daten aus form Data als "ANY"
         let query = new URLSearchParams(formData);
+        //url + query zusammen
         url = url + "?" + query.toString();
         console.log(query.toString());
+        //Antwort des Servers aus url+query
         await fetch(url);
         for (let entry of query) {
             console.log(entry);
