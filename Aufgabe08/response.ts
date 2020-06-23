d3c46b5325882aff1b565e9d9b64f746ece4298a
ChillyVanilly <vasilii.gurev@hs-furtwangen.de>
2020-06-23 19:02:56 +0200

    Update response.ts

--- Aufgabe08/response.ts
+++ Aufgabe08/response.ts
@@ -1,13 +1,25 @@
 namespace Aufgabe08 {
+    //Gibt Button einen Eventlistener bei Click
     document.getElementById("buttonid")?.addEventListener("click", response);
     
     
     async function response(): Promise<void> {
+
+        //greift auf die Daten des forms im login.html zurück und packt sie in formDatra
         let formData: FormData = new FormData(document.forms[0]);
+
+        //Server url als String
         let url: string = "https://vasilii-server.herokuapp.com/";
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
+
+        //Quer : Daten aus form Data als "ANY"
+        let query: URLSearchParams = new URLSearchParams(formData.toString());
+
+        //url + query zusammen
         url = url + "?" + query.toString();
+        console.log("url: " + url);
         console.log(query.toString());
+
+        //Antwort des Servers aus url+query
         await fetch(url);
         for (let entry of query) {
             console.log(entry);
